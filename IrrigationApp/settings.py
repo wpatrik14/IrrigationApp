fd34f8a5a6d750503a2f4b2a7f0e9bd9be90da66
Weisz Patrik <wpatrik14@gmail.com>
2015-02-05 17:36:01 +0100

    settings

--- IrrigationApp/settings.py
+++ IrrigationApp/settings.py
@@ -110,5 +110,8 @@ CELERYBEAT_SCHEDULE = {
     },
 }
 CELERY_TIMEZONE = 'Europe/London'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 djcelery.setup_loader()
