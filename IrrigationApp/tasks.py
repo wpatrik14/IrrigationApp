92db2403bfa0062521aa5493150e01122ee1517b
Weisz Patrik <wpatrik14@gmail.com>
2015-02-05 17:19:06 +0100

    Unused import

--- IrrigationApp/tasks.py
+++ IrrigationApp/tasks.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 from celery import Celery
-from urllib.request import urlopen
 import codecs
 from datetime import datetime
 from IrrigationApp.models import WeatherHistory, Sensor, Switch, Segment
