8707e3e95242aed57047dd998309089cecd94c92
Weisz Patrik <wpatrik14@gmail.com>
2015-02-10 10:36:32 +0100

    asd

--- IrrigationApp/tasks.py
+++ IrrigationApp/tasks.py
@@ -12,9 +12,7 @@ from urllib.request import urlopen
 
 app = Celery('tasks', backend="amqp", broker='amqp://guest@localhost:5672//', include=['celery.task.http'])
 
-@app.task
-def get_weather_datas():
-    
+def get_weather_data_from_server():
     res = urlopen('http://api.worldweatheronline.com/free/v1/weather.ashx?q=God&format=json&num_of_days=5&key=bffc71ad3fa08458dbf6fc77a0383cd421d61052')
     reader = codecs.getreader("utf-8")
     js = json.load(reader(res))
@@ -113,6 +111,13 @@ def get_weather_datas():
         tempMin_C=tempMinC_4,
         icon=icon_4)
     mWeatherForecast_4.save()
+    
+    return
+
+@app.task
+def get_weather_datas():
+    
+    get_weather_data_from_server()
     
     return '\n\nGETTING WEATHER DATAS........... DONE'
 
@@ -215,6 +220,10 @@ def automation_control():
     
     segments = Segment.objects.all()
     weatherForecast = WeatherForecast.objects.all().order_by('-forecast_date')[:1]
+    
+    if weatherForecast is None :
+        get_weather_data_from_server()
+    
     for segment in segments :
         if segment.type == "Automatic" :
             if segment.sensor.status>segment.moisture_minLimit :
