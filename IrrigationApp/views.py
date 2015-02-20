5bef95032c3feb86303d0c5757e4f32270993bf6
Weisz Patrik <wpatrik14@gmail.com>
2015-02-20 08:34:18 +0100

    asd

--- IrrigationApp/views.py
+++ IrrigationApp/views.py
@@ -557,7 +557,7 @@ def doAddIrrigationTemplateValues(request):
     series = request.POST['series']
     
     reader = codecs.getreader("utf-8")
-    js = json.load(reader(series))
+    js = json.load(series)
     
     point1_x=js['data'][0]['x'];
     point1_y=js['data'][0]['y'];
