610e740abe4a589634f13a0d5d8c7a446fbdda90
Weisz Patrik <wpatrik14@gmail.com>
2015-03-23 15:59:51 +0100

    asd

--- IrrigationApp/settings.py
+++ IrrigationApp/settings.py
@@ -140,8 +140,16 @@ CELERYBEAT_SCHEDULE = {
 }
 
 CELERY_TIMEZONE = 'Europe/Budapest'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+from kombu.serialization import register
+from .myjson import my_dumps, my_loads
+
+register('myjson', my_dumps, my_loads, 
+    content_type='application/x-myjson',
+    content_encoding='utf-8') 
+
+# Tell celery to use your new serializer:
+CELERY_ACCEPT_CONTENT = ['myjson']
+CELERY_TASK_SERIALIZER = 'myjson'
+CELERY_RESULT_SERIALIZER = 'myjson'
 
 djcelery.setup_loader()
