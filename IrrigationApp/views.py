443cb89d89f0d0e984b43a9c481a6a548131d2ea
Weisz Patrik <wpatrik14@gmail.com>
2015-10-17 14:16:56 +0200

    asd

--- IrrigationApp/views.py
+++ IrrigationApp/views.py
@@ -234,8 +234,9 @@ def setIrrigation(mSegment, status):
     mSegment.switch=mSwitch
     mSegment.save(update_fields=['switch','up_time','irrigation_history']) 
     #urlopen("http://"+arduino.IP+":"+arduino.PORT+"/pinNumber/"+mSwitch.pinNumber+"/status/"+str(mSwitch.status))
-    pipe = subprocess.Popen(['/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/radiomodule', '1', '0', str(mSwitch.pinNumber), str(mSwitch.status)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout, stderr = pipe.communicate()
+    pipe = subprocess.Popen(['sudo','/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/radiomodule', '1', '0', str(mSwitch.pinNumber), str(mSwitch.status)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    f = open('/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/output.txt','r')
+    result = f.read()
         
     switches = Switch.objects.all()
     running_segments=0;
@@ -256,11 +257,11 @@ def setIrrigation(mSegment, status):
     settings.running_segments=running_segments
     settings.save(update_fields=['running_segments'])
     
-    pipe = subprocess.Popen(['/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/radiomodule', '1', '0', str(pump.switch.pinNumber), str(pump.switch.status)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #stdout, stderr = pipe.communicate()
-    #urlopen("http://"+arduino.IP+":"+arduino.PORT+"/pinNumber/"+pump.switch.pinNumber+"/status/"+str(pump.switch.status))
+    pipe = subprocess.Popen(['sudo','/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/radiomodule', '1', '0', str(pump.switch.pinNumber), str(pump.switch.status)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #f = open('/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/output.txt','r')
+    #result = f.read()
     
-    return stderr
+    return result
     
 def addTaskToQueue(mSegment):
     settings = IrrigationSettings.objects.all()
