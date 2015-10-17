c7ce7c5693ddb37606554611d6be8b7aa5476780
Weisz Patrik <wpatrik14@gmail.com>
2015-10-17 15:03:14 +0200

    asd

--- IrrigationApp/tasks.py
+++ IrrigationApp/tasks.py
@@ -259,20 +259,20 @@ def changeSchedule(schedule, status):
 @task()
 def automation_control():
     reader = codecs.getreader("utf-8")
-    
+    result=""
     subprocess.Popen(['sudo','/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/radiomodule_withresponse', '0', '0', '0', '0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     time.sleep(3)
-    file = open('/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/output.txt','r')
-    result=file.read()
-    js = json.load(reader(str(result)))
-    Switch(pinNumber=int(js['Pin']),status=int(js['Stat'])).save()
+    with open('/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/output.txt','r') as file:
+        result=str(file.read())
+        js = json.loads(reader(result))
+        Switch(pinNumber=int(js['Pin']),status=int(js['Stat'])).save()
     
     subprocess.Popen(['sudo','/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/radiomodule_withresponse', '0', '0', '1', '0'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     time.sleep(3)
-    file = open('/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/output.txt','r')
-    result=file.read()
-    js = json.load(reader(str(result)))
-    Switch(pinNumber=int(js['Pin']),status=int(js['Stat'])).save()
+    with open('/home/pi/rf24libs/stanleyseow/RF24/RPi/RF24/examples/output.txt','r') as file:
+        result=str(file.read())
+        js = json.loads(reader(result))
+        Switch(pinNumber=int(js['Pin']),status=int(js['Stat'])).save()
     
     for i in range(node_counts) :
 
