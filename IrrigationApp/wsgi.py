a4c030b6c1f4dae2e3b0b7800d942f2d68a7fdbf
Weisz Patrik <wpatrik14@gmail.com>
2015-11-21 16:15:09 +0100

    asd

--- IrrigationApp/wsgi.py
+++ IrrigationApp/wsgi.py
@@ -27,7 +27,7 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    message = str(msg.payload).split(sep="'")
+    message = str(msg.payload).split("'")
     js = json.loads(message[1])
     
     zone = js['zone']
