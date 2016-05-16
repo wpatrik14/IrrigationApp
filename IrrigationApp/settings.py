a0bbfd28697484894620951b8771453de31109e0
Weisz Patrik <wpatrik14@gmail.com>
2016-05-16 11:38:06 +0200

    asd

--- IrrigationApp/settings.py
+++ IrrigationApp/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '465'
+EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'noriespatrik@gmail.com'
 EMAIL_HOST_PASSWORD = 'WPatriot91'
 EMAIL_USE_TLS = True
