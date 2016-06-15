d3e43063b6f87a6c3343956e3886346432fec68d
Weisz Patrik <wpatrik14@gmail.com>
2016-06-15 18:08:58 +0200

    asd

--- IrrigationApp/settings.py
+++ IrrigationApp/settings.py
@@ -108,7 +108,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'godlocsolas@gmail.com'
-EMAIL_HOST_PASSWORD = 'bogi1995'
+EMAIL_HOST_PASSWORD = 'bogi1965'
 EMAIL_USE_TLS = True
 
 # Static files (CSS, JavaScript, Images)
